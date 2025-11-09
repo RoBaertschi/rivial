73676a633c6ff1e1851a0543ee52ca7a44aba4d4
RoBaertschi <rtmbaertschi007@gmail.com>
2025-11-09 19:52:57 +0100

    don't make case expressions constants

--- queries/c/highlights.scm
+++ queries/c/highlights.scm
@@ -4,5 +4,8 @@
 ((identifier) @RivialNormal
   (#lua-match? @RivialNormal "^[A-Z][A-Z0-9_]+$"))
 
+(case_statement
+  value: (identifier) @RivialNormal)
+
 (enumerator
   name: (identifier) @constant)
